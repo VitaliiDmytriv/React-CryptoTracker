70e25ff7ef1bc98b00db8db30e6f347104a6c199
Vitalii Dmytriv <vitaliqq.dmutriv@gmail.com>
2026-01-01 14:40:38 +0200

    prisma init

---
+++ server/src/types/global.ts
@@ -0,0 +1,42 @@
+export interface DataBase {
+  users: User[];
+}
+
+export interface User {
+  id: string;
+  userName: string;
+  email: string;
+  password: string;
+  portfolio: Portfolio;
+}
+
+export interface Portfolio {
+  totalProfit: number;
+  activeInvestment: number;
+  coins: Record<string, Coin>;
+}
+
+export interface Transaction {
+  symbol: string;
+  name: string;
+  id: string;
+  image: string;
+  quantity: number | null;
+  pricePerCoinBought: number | null;
+  fees: number | null;
+  totalSpent: number | null;
+  pricePerCoinSold: null | number;
+  profit: null | number;
+  date: string;
+}
+
+export interface Coin {
+  name: string;
+  symbol: string;
+  image: string;
+  totalProfit: number;
+  activeInvestment: number;
+  holdings: number;
+  avgPrice: number;
+  transactions: Transaction[];
+}
